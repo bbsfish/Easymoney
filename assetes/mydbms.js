fd79ef34b5ced72fd1a2086f270f78aca400e7f3
bbsfish <ysk1122.dev@gmail.com>
2023-07-13 23:49:51 +0900

    mydbms アプデート

--- assetes/mydbms.js
+++ assetes/mydbms.js
@@ -12,100 +12,113 @@
 
 class MyDbms {
     constructor(book_id = "", sheet_name = "") {
-        this.ssid = book_id;
-        this.sname = sheet_name;
-        this.cmx = -1;
-        this.data = null;
-        this.ssBook = null;
-        this.ssSheet = null;
+      this.ssid = book_id;
+      this.sname = sheet_name;
+      this.cmx = -1;
+      this.data = null;
+      this.ssBook = null;
+      this.ssSheet = null;
     };
-
+  
+    // Return = SpreadsheetApp Object ;
     init() {
-        if (this.ssBook == null) {
-            this.ssBook = SpreadsheetApp.openById(this.ssid);
-            this.ssSheet = this.ssBook.getSheetByName(this.sname);
-        }
-        if (this.cmx == -1) {
-            this.cmx = this.ssSheet.getLastColumn();
-        }
-        return this.ssSheet;
+      if (this.ssBook == null) {
+        this.ssBook = SpreadsheetApp.openById(this.ssid);
+        this.ssSheet = this.ssBook.getSheetByName(this.sname);
+      }
+      if (this.cmx == -1) {
+        this.cmx = this.ssSheet.getLastColumn();
+      }
+      return this.ssSheet;
     };
-
+  
+    // Return = null | [[]] ;
     read() {
-        const sheet = this.init();
-        const lastRow = sheet.getLastRow(); // 最終行数取得
-        if (lastRow <= 1) return; // データなしのとき
-        return sheet.getRange(2, 1, lastRow - 1, this.cmx).getValues(); // データ取得 ID列から最終列
+      const sheet = this.init();
+      const lastRow = sheet.getLastRow();
+      if (lastRow <= 1) return null;
+      return sheet.getRange(2, 1, lastRow - 1, this.cmx).getValues(); // [A] ~ last
     };
-
-    stack(strict = false /* false: 読み速度優先, true: 正確さ優先 */) {
-        if (strict || this.data == null) {
-            // データロード  正確モード||データロード未実施
-            this.data = this.read();
-        }
-        return this.data;
+    
+    // Return = null | [[]] ;; Args = strict: boolean (false*: 読み速度優先, true: 正確さ優先)
+    stack(strict = false) {
+      if (strict || this.data == null) {
+        // データロード  正確モード||データロード未実施
+        this.data = this.read();
+      }
+      return this.data;
     };
-
-    // 戻り値:: Null | [] | [[]] :: 検索ID = "{ID}" | "*", 検索モード = false (読み速度優先: default) | true (正確さ優先)
-    select(id = "", strict = false /* false: 読み速度優先, true: 正確さ優先 */) {
-        if (strict) {
-            // 正確モード
-            const sheet = this.init();
-            const lastRow = sheet.getLastRow(); // 最終行の取得
-            if (lastRow <= 1) return null; // データなしのとき
-            const datas = sheet.getRange(2, 1, lastRow - 1, this.cmx).getValues(); // データ取得 ID列から最終列
-            // データの検索
-            if (id == "*") {
-                return datas;     // すべて返却[[],[],[]]
-            } else {
-                let n_data = datas.filter(function (value) { return value[0] == id });
-                return n_data[0]; // 指定IDのみ返却[[]]
-            }
-        } else {
-            // 高速モード
-            const datas = this.stack(); // データ取得
-            if (!datas) return null;    // データなしのとき
-            // データの検索
-            if (id == "*") {
-                return datas;     // すべて返却[[],[],[]]
-            } else {
-                let n_data = datas.filter(function (value) { return value[0] == id });
-                return n_data[0];    // 指定IDのみ返却[]
-            }
-        }
-    }
-
-    // 戻り値:: true | false :: 検索ID = "{ID}", 更新したいカラムデータ配列 = [] (ID, Update列を除いたカラムサイズに一致必須)
-    update(id = "", new_cols_array = []) {
+  
+    // Return = null | [] ;; Args = id: String (検索ID), strict: boolean (false*: 読み速度優先, true: 正確さ優先)
+    select(id = "", strict = false) {
+      let datas = null;
+      if (strict) {
+        // 正確モード
         const sheet = this.init();
-        if (new_cols_array.length < this.cmx - 2) return false;
-        let dd = new Date();
-        let appendQuery = [id, dd].concat(new_cols_array);
-        // 最終行の取得
         const lastRow = sheet.getLastRow();
-        if (lastRow <= 1) return false;
-        // データの取得
-        const datas = sheet.getRange(2, 1, lastRow - 1, this.cmx - 2).getValues();
-        // データの検索
-        const dataIndex = datas.findIndex((value) => { return value[0] == id });
-        if (dataIndex < 0) return false;  // データがマッチしない場合は除外
-        sheet.getRange(dataIndex + 2, 1, 1, this.cmx).setValues([appendQuery]); // データ更新 ID列から最終列
-        return true;
+        if (lastRow <= 1) return null;
+        datas = sheet.getRange(2, 1, lastRow - 1, this.cmx).getValues(); // [A] ~ last
+      } else {
+        // 高速モード
+        datas = this.stack(); // [A] ~ last, 全取得[[]]
+      }
+      if (!datas) return null;
+      for (let d of datas) { if (d[0] == id) return d; }
     }
-
-    // 戻り値:: true | false :: 検索ID = "{ID}", 削除したい行のID = "{ID}"
+  
+    // Return = null | [[]] ;; Args = id: String (検索ID), strict: boolean (false*: 読み速度優先, true: 正確さ優先)
+    selects(id = "", strict = false) {
+      let datas = null;
+      if (strict) {
+      // 正確モード
+        const sheet = this.init();
+        const lastRow = sheet.getLastRow();
+        if (lastRow <= 1) return null;
+        datas = sheet.getRange(2, 1, lastRow - 1, this.cmx).getValues(); // [A] ~ last
+      } else {
+      // 高速モード
+        datas = this.stack();
+      }
+      if (!datas) return null;
+      if (id == "*") { return datas; }
+      else { return datas.filter((v) => { return v[0] == id }); }
+    }
+  
+    // Return = boolean ;; Args = id: String (検索ID), new_row_array: Array (更新したいデータ内容)
+    update(id = "", new_row_array = []) {
+      const sheet = this.init();
+      if (new_row_array.length < this.cmx - 2) return false;
+      let dd = new Date();
+      let appendQuery = [id, dd].concat(new_row_array);
+      const lastRow = sheet.getLastRow();
+      if (lastRow <= 1) return false;
+      const datas = sheet.getRange(2, 1, lastRow - 1, this.cmx - 2).getValues();
+      const dataIndex = datas.findIndex((v) => { return v[0] == id });
+      if (dataIndex < 0) return false;  // データがマッチしない場合は除外
+      sheet.getRange(dataIndex + 2, 1, 1, this.cmx).setValues([appendQuery]); // データ更新 [A] ~ last
+      return true;
+    }
+  
+    // Return = boolean ;; Args = id: String (対象行のID)
     remove(id = "") {
-        const sheet = this.init();
-        // 最終行の取得
-        const lastRow = sheet.getLastRow();
-        if (lastRow <= 1) return false;
-        // データの取得
-        const datas = sheet.getRange(2, 1, lastRow - 1, 1).getValues();
-        // データの検索
-        const dataIndex = datas.findIndex((value) => value == id)
-        // データがマッチしない場合は除外
-        if (dataIndex < 0) return false;
-        sheet.deleteRow(dataIndex + 2);
-        return true;
+      const sheet = this.init();
+      const lastRow = sheet.getLastRow();
+      if (lastRow <= 1) return false;
+      const datas = sheet.getRange(2, 1, lastRow - 1, 1).getValues();
+      const dataIndex = datas.findIndex((value) => value == id)
+      if (dataIndex < 0) return false; // データがマッチしない場合は除外
+      sheet.deleteRow(dataIndex + 2);
+      return true;
     }
-}
+  
+    // Return = boolean ;; Args = id: String (検索ID), new_row_array: Array (挿入したいデータ内容)
+    insert(new_row_array = [], new_id = "") {
+      const sheet = this.init();
+      if (new_row_array.length >= this.cmx - 2) return false;
+      let dd = new Date();
+      let id = (new_id!="") ? new_id : Utilities.getUuid();
+      let appendQuery = [id, dd].concat(new_row_array);
+      sheet.appendRow(appendQuery);
+      return true;
+    }
+  }
